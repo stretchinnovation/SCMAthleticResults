f2a1195e97b959ab26b7246fbe22f06fab6e3d71
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-20 10:43:05 +0200

    Update AthleticResults.py

--- AthleticResults.py
+++ AthleticResults.py
@@ -55,7 +55,8 @@ if files:
 
     # Specify which columns you want to show
     columns_to_show = ["Place", "Surname", "Firstname", "Number", "Team", "Performance"]
-    st.dataframe(df2[columns_to_show])
+    #st.dataframe(df2[columns_to_show])
+    
     # Load Poppins font globally
     st.markdown(
         """
